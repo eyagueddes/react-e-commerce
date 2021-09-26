0d1b9df14d7899bbd4d2b733ee05e130d58d6095
eya gueddes <eyagueddes1@gmail.com>
2021-09-26 22:31:34 +0100

    third

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,12 @@
 const express = require('express'); 
 const router=express.Router();
 const auth=require('../../middlewares/auth');
-const User=require('../../models/User')
+const User=require('../../models/User');
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const config=require('config')
+const {check,validationResult}=require('express-validator');
+
 router.get('/',auth,async(req,res)=>
 {
     try {
@@ -15,6 +20,59 @@ router.get('/',auth,async(req,res)=>
     }
 }
 );
+
+router.post('/',[
+    check('email','please enter the email').isEmail(),
+
+    check('password','password is required').exists(),
+],
+
+async (req,res)=> {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const{name,email,password}=req.body;
+ 
+    
+    //check if user exist
+    try {
+        let user= await User.findOne({email});
+        if(!user){
+            res.status(400).json({errors:[{msg:'invalid credentials'}]});
+        }
+
+//check if the password match that user
+      const isMatch=await bcrypt.compare(password,user.password);
+      if(!isMatch){
+        res.status(400).json({errors:[{msg:'invalid credentials'}]});
+
+      }
+
+    //return Jsonwebtoken
+    const payload={
+        user:{
+            id:user.id
+        }
+    };
+    jwt.sign(payload,config.get('jwtSecret'),
+    { expiresIn:360000},
+    (error,token)=>{
+        if(error) throw error;
+        res.json({token});
+    }
+    ); 
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('server error');    
+    }
+    
+
+ } );
+
+
 
 
 
